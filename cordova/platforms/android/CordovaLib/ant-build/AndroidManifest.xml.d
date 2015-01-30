b6fc504d845f1c3bef07d4b577388d27fa101c17
Armando Gonzalez <iam@armando.mx>
2015-01-29 19:37:51 -0600

    removed cordova folder

--- cordova/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/manduks/Proyectos/Sencha/Triton1.1/cordova/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/manduks/Proyectos/Sencha/Triton1.1/cordova/platforms/android/CordovaLib/AndroidManifest.xml \
--- cordova/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/manduks/Proyectos/Sencha/Triton1.1/cordova/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/manduks/Proyectos/Sencha/Triton1.1/cordova/platforms/android/AndroidManifest.xml \
